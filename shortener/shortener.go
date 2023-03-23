97ae93e4c0a95081c3dee1ac4947ddf05790af31
pawlik <grzegorz.pawlik@toptal.com>
2023-03-23 23:20:56 +0100

    Add 'hashing' module

---
+++ shortener/shortener.go
@@ -0,0 +1,74 @@
+package shortener
+
+import (
+	"regexp"
+)
+
+// first letter: len of URL % 52 = map a-ZA-Z
+// number of vals mod 52
+// number of consonants
+// if conflict = add more letters with this approach
+
+const (
+	upperCaseA  = 42
+	lowerCaseA  = 97
+	cardinality = 26
+)
+
+// TODO: to clarify later: All functions close on this var,
+// so we're not copying it at all? It's set once in `init`
+// and we read from the original
+var chars [cardinality * 2]rune
+
+func init() {
+	i := 0
+	for j := lowerCaseA; j < lowerCaseA+cardinality; j++ {
+		chars[i] = rune(j)
+		i++
+	}
+	for j := upperCaseA; j < upperCaseA+cardinality; j++ {
+		chars[i] = rune(j)
+		i++
+	}
+}
+
+// This is not something I'd implement in production,
+// just made up, probably mathematically incorrect
+// hashing function, with unknown collision probability
+// I just want to use go routines for that.
+func Shorten(in string) string {
+	len := make(chan string, 1)
+	val := make(chan string, 1)
+	cons := make(chan string, 1)
+
+	go lenBased(in, len)
+	go valBased(in, val)
+	go consBased(in, cons)
+
+	return <-len + <-val + <-cons
+}
+
+// picks character based on length
+func lenBased(in string, c chan string) {
+	c <- string(chars[(len(in))%cardinality])
+}
+
+// picks character based on number of vals
+func valBased(in string, c chan string) {
+	re := regexp.MustCompile(
+		`(?i)[aeiou]`,
+	)
+	matches := re.FindAllStringIndex(in, -1)
+
+	c <- string(chars[len(matches)%cardinality])
+}
+
+// picks character based on number of vals
+func consBased(in string, c chan string) {
+	re := regexp.MustCompile(
+		`(?i)[bcdfghjklmnpqrstvwxys]`,
+	)
+	matches := re.FindAllStringIndex(in, -1)
+
+	c <- string(chars[len(matches)%cardinality])
+}
---
+++ shortener/shortener_test.go
@@ -0,0 +1,30 @@
+package shortener
+
+import "testing"
+
+func TestFoo(t *testing.T) {
+	testCases := []struct {
+		Input    string
+		Expected string
+	}{
+		{
+			Input:    "a",
+			Expected: "bba",
+		},
+		{
+			Input:    "aa",
+			Expected: "cca",
+		},
+		{
+			Input:    "b",
+			Expected: "bab",
+		},
+	}
+
+	for _, tC := range testCases {
+		got := Shorten(tC.Input)
+		if got != tC.Expected {
+			t.Errorf("for %s expected %s got %s", tC.Input, tC.Expected, got)
+		}
+	}
+}
